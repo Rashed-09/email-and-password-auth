08701b7b06fa71d7ce63e97589a657a1eb5d84a8
Rashed-09 <rashedmdabdur452@gmail.com>
2024-11-21 12:15:54 +0600

    have added  rout setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,33 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Register from './components/Register/Register.jsx'
+import HeroRegister from './components/HeroRegister/HeroRegister.jsx'
+import Login from './components/Login/Login.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/heroRegister",
+        element: <HeroRegister></HeroRegister>
+      }
+    ],
   },
 ]);
 
